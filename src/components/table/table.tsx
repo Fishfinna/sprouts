1f46b50e7b374e413dbfee7915264d24953462bc
Shanti <fishfinna12@gmail.com>
2024-12-21 17:53:26 -0800

    change to table

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -12,11 +12,13 @@ export function Table() {
             <td>Alfreds Futterkiste</td>
             <td>Maria Anders</td>
             <td>Germany</td>
+            <td></td>
         </tr>
         <tr>
             <td>Centro comercial Moctezuma</td>
             <td>Francisco Chang</td>
             <td>Mexico</td>
+            <td></td>
         </tr>
     </table>);
 }
