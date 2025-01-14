a83b95ca2dc89068cc784243b9ba65a2f07d295b
Shanti <fishfinna12@gmail.com>
2025-01-13 19:42:42 -0800

    set up some of the styles for the upload button

--- src/components/file-manager/fileManager.tsx
+++ src/components/file-manager/fileManager.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import "./fieManager.scss";
 
 export interface Transaction {
   isSpending: boolean;
@@ -13,10 +14,17 @@ export function CSVReader({
 }: {
   setTransactions: (transactions: Transaction[]) => void;
 }) {
-  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+  const [fileMsg, setFileMsg] = useState("or drag and drop files here");
+  const [isActive, setIsActive] = useState(false);
 
-    if (file) {
+  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      setFileMsg(
+        files.length === 1 ? file.name : `${files.length} files selected`
+      );
+
       const reader = new FileReader();
       console.log(file.name);
 
@@ -53,9 +61,72 @@ export function CSVReader({
     }
   };
 
+  const handleDragEnter = () => setIsActive(true);
+  const handleDragLeave = () => setIsActive(false);
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsActive(false);
+
+    const files = event.dataTransfer.files;
+    if (files && files.length > 0) {
+      setFileMsg(
+        files.length === 1 ? files[0].name : `${files.length} files selected`
+      );
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target?.result as string;
+
+        // Parse the CSV
+        const rows = text.split("\n").map((row) => row.split(","));
+
+        // Get headers and create JSON array
+        const headers = rows[0];
+        console.log({ rows, headers });
+        const data: Transaction[] = rows.slice(1).map((row) => {
+          const values = row.map((value) => value.trim());
+          const transaction: Transaction = {
+            isSpending: values[0].toLowerCase() === "true",
+            category: values[1] || undefined,
+            date: values[2] || undefined,
+            isNeed:
+              values[3] === "true"
+                ? true
+                : values[3] === "false"
+                ? false
+                : null,
+            price: values[4] || undefined,
+          };
+          return transaction;
+        });
+
+        setTransactions(data);
+      };
+
+      reader.readAsText(files[0]);
+    }
+  };
+
   return (
-    <div>
-      <input type="file" accept=".csv" onChange={handleFileUpload} />
+    <div
+      className={`file-drop-area ${isActive ? "is-active" : ""}`}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragOver={(e) => e.preventDefault()}
+      onDrop={handleDrop}
+    >
+      <span className="fake-btn">
+        Import File <i className="material-icons">add</i>
+      </span>
+      <span className="file-msg">{fileMsg}</span>
+      <input
+        className="file-input"
+        type="file"
+        multiple
+        onChange={handleFileUpload}
+        onFocus={handleDragEnter}
+        onBlur={handleDragLeave}
+      />
     </div>
   );
 }
